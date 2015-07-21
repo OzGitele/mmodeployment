f3f086b390e6b675faab2236f5522e2d63c12f51
Oz <uzronen@microsoft.com>
2015-07-21 15:48:21 +0300

    ssss

--- mmofrontend/routes/webapi.js
+++ mmofrontend/routes/webapi.js
@@ -19,7 +19,7 @@ exports.getData = function (req, res) {
             console.log('Connection established to', connString);
 
             // do some work here with the database.
-            //var collection = db.collection('tasks');
+            var collection = db.collection('tasks');
             //var task1 = { itemName: 'Item7', itemCategory: 'Cat2', itemCompleted: false, itemDate: Date.now };
             //var task2 = { itemName: 'Item8', itemCategory: 'Cat2', itemCompleted: false, itemDate: Date.now };
             //var task3 = { itemName: 'Item9', itemCategory: 'Cat1', itemCompleted: false, itemDate: Date.now };
